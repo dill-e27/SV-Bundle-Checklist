8e25ed2aa6ea70542ca50be8e49e11c5727e0ddb
dill-e27 <dylan92008@gmail.com>
2025-08-02 12:59:16 -0230

    Update BundleSection.jsx

--- src/components/BundleSection.jsx
+++ src/components/BundleSection.jsx
@@ -21,7 +21,7 @@ function BundleSection({ bundleName, items, checkedItems, setCheckedItems }) {
               <strong>{item.name}:</strong> {item.description}
             </label>
             <img
-              src={`/images/${item.image}`}
+              src={`/images/Items/${item.image}`}
               alt={item.name}
               className="item-image"
             />
@@ -33,5 +33,6 @@ function BundleSection({ bundleName, items, checkedItems, setCheckedItems }) {
 }
 
 export default BundleSection;
+
 
 
